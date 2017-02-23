861db0ca84800ae8610d1f815c6317343cbccf8c
Hai Duc Nguyen <ndhai@node19.supernodexp.hpc>
2017-02-23 20:42:45 +0600

    create mutiple tests

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ OBJ = obj
 TESTS = tests
 INCLUDE = include
 
-OPT_BW = -mcx16 -Wno-invalid-offsetof -frename-registers -funroll-loops -flto -march=native -DNDEBUG -DBWTREE_NODEBUG
+OPT_BW = -mcx16 -Wno-invalid-offsetof -frename-registers -Ofast -funroll-loops -flto -march=native -DNDEBUG -DBWTREE_NODEBUG
 
 CC = g++
 STD = -std=c++11
@@ -30,9 +30,8 @@ TEST_BWTREE = tests/bw.cpp $(addprefix $(OBJ)/, bwtree.o)
 TEST_HUGE = tests/hugepage.cpp $(addprefix $(OBJ)/, mm.o)
 SERVER = tests/server.cpp $(addprefix $(OBJ)/, mm.o procs_base.o bnet.o layout.o) 
 TEST_UNI = tests/uni.cpp $(addprefix $(OBJ)/, mm.o bnet.o procs_base.o layout.o common.o)
-TEST_DGEN = tests/genit.cpp tests/rand.cpp $(addprefix $(OBJ)/, mm.o bnet.o procs_base.o layout.o common.o)
 VERBS = tests/verbs.cpp $(addprefix $(OBJ)/, mm.o hash.o larmdata.o common.o bnet.o intf.o hashtable.o) libs/libcityhash.a
-STRESS = $(addprefix $(OBJ)/, mm.o hash.o larmdata.o common.o bnet.o intf.o hashtable.o) libs/libcityhash.a
+STRESS = $(addprefix $(OBJ)/, mm.o bnet.o procs_base.o layout.o common.o)
 
 all:
 
@@ -59,22 +58,17 @@ uni: $(TEST_UNI)
 	$(MAKE) $(LFLAGS) $(TEST_UNI) -o $(ROOT)/$(TESTS)/run_test_uni $(LIB)
 	$(ROOT)/$(TESTS)/run_test_uni
 
-
-dgen: $(TEST_DGEN)
-	$(MAKE) $(LFLAGS) $(TEST_DGEN) -o $(ROOT)/$(TESTS)/run_dgen $(LIB)
-	$(ROOT)/$(TESTS)/run_dgen 0 1000000
-
 verb: $(VERBS)
 	$(MAKE) $(LFLAGS) $(VERBS) -o $(ROOT)/$(TESTS)/run_verb $(LIB)
 	$(ROOT)/$(TESTS)/run_verb
 
 genit: $(STRESS) tests/genit.cpp tests/rand.cpp
 	$(MAKE) $(LFLAGS) tests/genit.cpp tests/rand.cpp $(STRESS) -o $(ROOT)/$(TESTS)/run_genit $(LIB)
-	$(ROOT)/$(TESTS)/run_genit
+	$(ROOT)/$(TESTS)/run_genit 0 1000000
 
 stress: $(STRESS) tests/stress.cpp tests/rand.cpp
 	$(MAKE) $(LFLAGS) tests/stress.cpp tests/rand.cpp $(STRESS) -o $(ROOT)/$(TESTS)/run_stress $(LIB)
-	#$(ROOT)/$(TESTS)/run_stress
+	$(ROOT)/$(TESTS)/run_stress 0.1
 
 
 # Create objects from sources
--- tests/stress.cpp
+++ tests/stress.cpp
@@ -2,87 +2,54 @@
 #include <string>
 #include <iostream>
 #include <unistd.h>
-#include "larmdata.h"
 #include "net.h"
-#include "intf.h"
 #include "mm.h"
 #include "common.h"
 
-#include "indata.h"
 
 #include <chrono>
 #include <rand.h>
 #include <string.h>
+#include "procs.h"
+#include "layout.h"
+#include "query_base.h"
+#include <random>
 
 using namespace std;
 using namespace LARM;
-using namespace LARM::LARM_COMMON;
 using namespace LARM::LARM_NET;
 using namespace LARM::INLARM::LARM_MM;
 
 using namespace chrono;
 
-const unsigned int MAX_REQ = 1000000;
-const unsigned int MAX_CHAIN = 10;
+const unsigned int MAX_REQ = 1000;
 const unsigned int READ_RATE = 100;
 const unsigned int MAX_RATE = 100;
 
-const unsigned int KEY_SIZE = (80000000);
+const unsigned int DATA_SZ = (1000000);
 
 const bool skw = false;
 
 const unsigned int SIZE_CHANGE[4] = {
-	0, 	// add new attributes
-	35, 	// change data
-	35, 	// change links
-	0	// remove an item
+	50, 	// add new attributes
+	50, 	// change data
 };
 
-enum data_operations { ADDATTR, CHDATA, CHLINK, REIT };
-
-string const default_chars =
-	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789";
-
-string gen_string(int len, string const &allowed_char = default_chars) {
-	std::mt19937_64 gen {std::random_device()() };
-	std::uniform_int_distribution<size_t> dist
-		{0, allowed_char.length() - 1};
-	if (len <= 10) {
-		len = 100;
-	}else{
-		std::normal_distribution<> ldist(len, len >> 2);
-		do {
-			len = ldist(gen);
-		}while(len <= 0);
-	}
-	string str;
-	std::generate_n(std::back_inserter(str), len,
-		[&] { return allowed_char[dist(gen)]; });
-	return str;
-
-}
-
-uint32_t gen_slen(uint32_t len) {
-	if (len <= 10) {
-		return 100;
-	}else{
-		std::mt19937_64 gen {std::random_device()() };
-		std::normal_distribution<> ldist(len, len >> 2);
-		do {
-			len = ldist(gen);
-		}while (len <= 0);
-		return len;
-	}
-}
+enum data_operations { ADDATTR, REIT };
 
 zipf_generator * zg;
 
-uint64_t get_key();
+uint64_t get_key(uint64_t band);
 bool make_change();
 data_operations change_method();
 
 int main(int argc, char ** argv) {
 	
+	if (argc != 2) {
+		return 1;
+	}
+	uint64_t key_range = stod(argv[1]) * DATA_SZ;
+
 	LARM::INLARM::LARM_MM::init_mem(2);
 	
 	zg = new zipf_generator(0, KEY_SIZE, 0.99);
@@ -90,120 +57,75 @@ int main(int argc, char ** argv) {
 	add_host("10.2.1.21", "12345");
 	start_net(time_us() % (1 << 14) + (1 << 14),
 			(LARM::BYTE)get_base(), 2, processor, true);
+	
+	dbint_base<uint64_t, item_t, rquery<uint64_t>> dbint;
 
-	unsigned long num_chase = 0;
-	time_t time_chase = 0;
-	//double rp = 0.1;
-	unsigned long num_chain = 0;
+	unsigned long num_reads = 0;
+	time_t time_read = 0;
+	double rp = 0.1;
+	unsigned long num_items = 0;
 
 	cout << time_us() << endl;
 
 	for (unsigned int i = 0; i < MAX_REQ; ++i) {
-		uint64_t k = get_key();
-		lkey key((LARM::BYTE)&k, sizeof(k));
-		litem item;
-		//cout << i << endl;
-		if (!get_item(key, item)) {
-			if (make_change()) {
-				litem::builder b;
-				b.set_key(lkey((BYTE)&k, sizeof(int)));
-				b.set_int("num", 2);
-				k = get_key();
-				b.set_link("0", lkey((BYTE)&k, sizeof(int)));
-				k = get_key();
-				b.set_link("1", lkey((BYTE)&k, sizeof(int)));
-				b.set_str("data", gen_string(100));
-				put_item(b.build());
-			}else{
-				i--;
-			}
-			continue;
-		}
 		if (make_change()) {
-			int num = *(int*)item.get_attr("num");
-			litem::builder builder(item);
-			uint64_t tk = get_key();
-			lkey data((LARM::BYTE)&tk, sizeof(tk));
+			uint64_t key = get_key(0);
 			switch (change_method()) {
-			case CHDATA: {
-				int len = string((char*)
-					item.get_attr("data")).length();
-				len = gen_slen(len);
-				char * data = new char [len];
-				builder.set_str("data", string(data, len));
-				put_item(builder.build());
-				delete data;
-				break;
-			}
 			case REIT:
-				remove_item(item.get_key());
-				break;
-			case CHLINK:
-				builder.set_link(to_string(
-					time_us() % num), data);
-				put_item(builder.build());
+				dbint.remove(key);			
 				break;
 			case ADDATTR:
-				if (num > 1000) {
-					i--;
-					break;
-				}
-				builder.set_link(to_string(num), data);
-				builder.set_int("num", num + 1);
-				put_item(builder.build());
+				dbint.insert(key, item_t(key, key));
 				break;
 			}
 		}else{
-			for (unsigned int j = 0; j < MAX_CHAIN; ++j) {
-				int num = *(int*)item.get_attr("num");
-				litem record;
-				string index = to_string(time_us() % num);
-				//cout << index << " " << num << endl;
+			uint64_t skey = get_key(key_range);
+			uint64_t ekey = skey + key_range - 1;
+			rquery<uint64_t> query(skey, ekey);
+			auto cursor = dbint.find(query);
+
+			while (cursor.valid()) {
 				auto start = high_resolution_clock::now();
-				bool chase = item.chase_link(index, record);
+				cursor.next();	
 				auto end = high_resolution_clock::now();
 				auto dur = duration_cast<nanoseconds>
 					(end - start);
-				time_chase += dur.count();
-				num_chase++;	
-				if (chase) {
-					item = record;
-				}else{
-					break;
-				}
+				time_read += dur.count();
+				num_items++;	
 			}
-			num_chain++;
+			num_reads++;
 		}
-		/*
+		
 		if ((double)i / (double)MAX_REQ > rp) {
 			cout << "Processed: " << (rp * 100) << "%" << endl;
 			rp += 0.1;
 		}
-		*/
+		
 	}
 
 	cout << time_us() << endl;
-	/*
-	cout << "Time: " << time_chase << " ns" << endl;
-	cout << "Number of chains: " << num_chain << endl;
-	cout << "Number of chases: " << num_chase << endl;
-	*/
-	cout << time_chase << endl;
-	cout << num_chain << endl;
-	cout << num_chase << endl;
-	cout << "Throughput: " << ((double)num_chase / (double)time_chase *
-		1e9) << " chases/s" << endl;
+	
+	cout << "Time: " << time_read << " ns" << endl;
+	cout << "Number of items: " << num_items << endl;
+	cout << "Number of reads: " << num_reads << endl;
+	
+	cout << time_read << endl;
+	cout << num_items << endl;
+	cout << num_reads << endl;
+	cout << "Throughput: " << ((double)num_items / (double)time_read *
+		1e9) << " items/s" << endl;
 	
 	end_net();
 
 }
 
-uint64_t get_key() {
+uint64_t get_key(uint64_t band) {
 	if (skw) {
 		return zg->next();
 	}else{
 		std::mt19937_64 gen {std::random_device()() };
-		std::uniform_int_distribution<size_t> dist {0, KEY_SIZE - 1};
+		std::uniform_int_distribution<size_t>
+			dist {0, DATA_SZ - band - 1};
 		//return dist(gen);
 		uint32_t data = dist(gen);
 		return data;
@@ -216,17 +138,11 @@ bool make_change() {
 }
 
 data_operations change_method() {
-	unsigned int method = time_us() % (SIZE_CHANGE[0] + SIZE_CHANGE[1] +
-		SIZE_CHANGE[2] + SIZE_CHANGE[3]);
+	unsigned int method = time_us() % (SIZE_CHANGE[0] + SIZE_CHANGE[1]);
 	data_operations data = ADDATTR;
 	if (method < SIZE_CHANGE[0]) {
 		data = ADDATTR;
 	}else if (method < SIZE_CHANGE[0] + SIZE_CHANGE[1]) {
-		data = CHDATA;
-	}else if (method < SIZE_CHANGE[0] + SIZE_CHANGE[1] + SIZE_CHANGE[2]) {
-		data = CHLINK;
-	}else {
-		cout << "data" << endl;
 		data = REIT;
 	}
 	return data;
